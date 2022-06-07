40e29dccec9c7d65ed7d66eae111ccc2fd51c783
Nicolas Molina <nicolasjmolina1@gmail.com>
2022-06-07 20:04:11 -0300

    basic edit profile

--- api/controllers/profile.controller.js
+++ api/controllers/profile.controller.js
@@ -31,9 +31,38 @@ const getUserProfile = async(req, res = response) => {
             message: 'Error request by bad token'
         });
     }
+}
 
+const editUserProfile = async(req, res = response) => {
+    const token = req.headers['auth-token'];
+    const { payload } = req.body;
+
+    if(token == '1234567'){
+        const profileService = new ProfileService();
+        const userProfileEdited = await profileService.editUserProfile(token, payload);
+        let response;
+        if(userProfileEdited.was_edited){
+            response = res.status(200).json({
+                was_edited: userProfileEdited.was_edited,
+                user_data_edited: userProfileEdited.user_new_data,
+                message: 'User was edited successfully'
+            });
+        }else{
+            response = res.status(200).json({
+                was_edited,
+                user_data_edited: userProfileEdited.user_new_data,
+                message: 'User was no edited'
+            });
+        }
+
+    }else{
+        return res.status(400).json({
+            message: 'Error request by bad token'
+        });
+    } 
 }
 
 module.exports = {
-    getUserProfile
+    getUserProfile,
+    editUserProfile
 }
--- api/routes/profile.routes.js
+++ api/routes/profile.routes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { getUserProfile } = require('../controllers/profile.controller');
+const { getUserProfile, editUserProfile } = require('../controllers/profile.controller');
 
 const router = Router();
 
 //add express-validator
 router.get('/user-profile', getUserProfile);
+
+router.put('/edit-profile', editUserProfile);
 
 
 module.exports = router;
